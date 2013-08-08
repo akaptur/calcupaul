7d6f9d9e9d120a633d16ef08cceb9d8a6244e935
Allison Kaptur <allison.kaptur@gmail.com>
2013-08-08 09:53:47 -0400

    regular expressions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import re
 
 class Calculator(object):
     def __init__(self):
@@ -11,22 +12,13 @@ class Calculator(object):
         second_number   = ''
         operator_found  = False
 
-        for char in expression:
-            if char in self.operators:
-                self.next_operation = char
-                operator_found = True
-            elif char.isdigit():
-                if not operator_found:
-                    first_number += char
-                else:
-                    second_number += char
-            else:
-                return "Invalid input"
-                
+        m = re.match('(\d+)(\D)(\d+)', expression)
+        if m is None:
+            return "Invalid input"
+        first_number, op, second_number = m.groups()
         first_number            = int(first_number)
         second_number           = int(second_number)
-        operation_function      = self.operators[self.next_operation]
+        operation_function      = self.operators[op]
         
         answer  = operation_function(first_number, second_number)
         return answer
-
